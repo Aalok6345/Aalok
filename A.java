cdde6c32da2df3c005ece6177c360c3cf79f938e
Aalok6345 <bca1stsem.160.aalokkumar@gmail.com>
2023-09-19 16:59:43 +0530

    first commit

--- A.java
+++ A.java
@@ -3,5 +3,9 @@ class A
 	public static void main(String[] args) 
 	{
 		System.out.println("hello aalok  rai");
+		System.out.println("hello aalok  rai");
+		System.out.println("hello aalok  rai");
+		System.out.println("hello aalok  rai");
+		System.out.println("hello aalok  rai");
 	}
 }
